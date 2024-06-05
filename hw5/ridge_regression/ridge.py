99810426395420fce9cd7fbdfe0c78011b8124fc
aakarsh nair <aakarsh@nentei.com>
2024-06-05 18:13:24 +0200

    Work towards adding hw5 plots.

--- hw5/ridge_regression/ridge.py
+++ hw5/ridge_regression/ridge.py
@@ -1,6 +1,7 @@
 #%%
 import numpy as np
 from scipy.optimize import minimize
+import matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -11,21 +12,25 @@ sys.path.append(os.path.abspath(os.path.join('..')))
 import ridge_regression.provided_functions as pf
 
 sns.set_style('whitegrid')
+FONT_SIZE= 16
+font ={ 'size'   : FONT_SIZE}
 
+matplotlib.rc('font', **font)
 #%%
 def LeastSquares(X, Y):
     ''' solves linear regression with
     L2 Loss
 
-    X: deisgn matrix n x d
+    X: design matrix n x d
     Y: true values n x 1
 
     output: weight of linear regression d x 1
     '''
+    # TODO: To use inverse or use solve ?
     return np.linalg.inv(X.T @ X) @ X.T @ Y
 
 #%%
-def RidgeRegression(D, Y, lmbd_reg=0.):
+def RidgeRegression(Phi, Y, lmbd_reg=0.):
     ''' solves linear regression with
     L2 Loss + L2 regularization
 
@@ -35,7 +40,11 @@ def RidgeRegression(D, Y, lmbd_reg=0.):
 
     output: weight of linear regression d x 1
     '''
-    return np.linalg.inv(D.T @ D + lmbd_reg * np.eye(D.shape[1])) @ D.T @ Y
+    d = Phi.shape[1]
+    I_d = np.eye(d)
+    # TODO: To use inverse or use solve?
+    # Returns weight vector of size d x 1
+    return np.linalg.inv(Phi.T @ Phi + lmbd_reg * I_d) @ Phi.T @ Y
 
 #%%
 def Basis(X, k):
@@ -85,24 +94,62 @@ def run_plot(data, train=False, test=False):
 def learned_weights(X, Y, k, lmbd_reg=0., type='ridge'):
    ''' learned_weights: learn the weights of the model, from training data with 
    k basis functions, and regularization parameter lmbd_reg.'''
-   D = Basis(X, k)
+   Phi = Basis(X, k)
    if type == 'ridge':
-    return  RidgeRegression(D, Y, lmbd_reg)
+    return  RidgeRegression(Phi, Y, lmbd_reg)
    elif type == 'l1':
-        return pf.L1LossRegression(D, Y, lmbd_reg)
+        return pf.L1LossRegression(Phi, Y, lmbd_reg)
    raise ValueError('Unknown type: %s'%type)
 
-#%%
-if __name__ == '__main__':
-    data = np.load('../data/onedim_data.npy', allow_pickle=True).item()
-    run_plot(data,train=True)
-    run_plot(data,test=True)
-    for type in ['ridge', 'l1']:
+def plot_comparison_l1_vs_ridge(data):
+    for k in [1,2, 3,5, 10, 15,20]:
         plt.figure(figsize=(7, 7))
         plt.scatter(data['Xtrain'], data['Ytrain'],s=1,cmap='viridis',label='Training Data')
-        for k in [1,2, 3,5, 10, 15,20]:
+        for type in ['ridge', 'l1']:
             w = learned_weights(data['Xtrain'], data['Ytrain'],k=k, lmbd_reg=30, type=type)
             x = np.linspace(0, 1, 1000)
             phi = Basis(x, k)
-            plt.plot(x, phi @ w, label='%s Loss Regression k=%d'%(type, k))
+            plt.plot(x, phi @ w, label='%s Loss Regression k=%d'%(type.upper(), k))
             plt.legend()
+        plt.savefig('../figures/l1_vs_ridge_regression_k_%d.png'%( k))
+
+def plot_l1_for_k(data, lmdb_reg=30):
+    type = 'l1'
+    fig = plt.figure(figsize=(10, 10))
+    plt.scatter(data['Xtrain'], data['Ytrain'],s=1,cmap='viridis',label='Training Data')
+    for k in [1,2, 3,5, 10, 15,20]:
+        w = learned_weights(data['Xtrain'], data['Ytrain'],k=k, lmbd_reg=lmdb_reg, type=type)
+        x = np.linspace(0, 1, 1000)
+        phi = Basis(x, k)
+        plt.plot(x, phi @ w, label='%s Loss Regression k=%d'%(type.upper(), k))
+        plt.legend()
+    plt.savefig('../figures/l1_regression_all_k.png')
+    return fig
+
+def plot_l1_loss_for_k(data):
+    type = 'l1'
+    fig = plt.figure(figsize=(10, 10))
+    K_VALS = [1,2, 3,5, 10, 15,20]
+    for lmdb_reg in [0, 30]:
+        for partition in ['train', 'test']:
+            l1_losses = np.zeros((len(K_VALS), 1))
+            for k_idx, k in enumerate(K_VALS):
+                w = learned_weights(data['Xtrain'], data['Ytrain'],k=k, lmbd_reg=lmdb_reg, type=type)
+                phi = Basis(data['X%s'%partition], k)
+                f_X = phi @ w
+                Y = data['Y%s' % partition] 
+                l1_loss_l2_reg = np.abs(f_X - Y).sum() + lmdb_reg * (w ** 2).sum()
+                l1_losses[k_idx] = l1_loss_l2_reg
+            plt.xlabel('k', fontsize=FONT_SIZE)    
+            plt.ylabel('Loss', fontsize=FONT_SIZE)
+            plt.plot(K_VALS,l1_losses, label='Losses : %s and Lambda: %d' % (partition.capitalize(), lmdb_reg))
+    plt.legend()
+    plt.savefig('../figures/l1_losses_for_k.png' )
+    return fig
+#%%
+if __name__ == '__main__':
+    data = np.load('../data/onedim_data.npy', allow_pickle=True).item()
+    plot_comparison_l1_vs_ridge(data)
+    plot_l1_for_k (data)
+    plot_l1_loss_for_k(data)
+#%%
